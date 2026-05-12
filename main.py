b93710a8c4b02e8df1587ee3fc89b9a707d82b30
S1mon <jaforsi.kozak@gmail.com>
2026-05-12 08:13:34 +0200

    updated compressor

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import numpy as np
 img = imageio.imread('data/8.png')
 
 
-import cv2
+#import cv2
 
-dims = (256, 256)
-fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-vid = cv2.VideoWriter('output.mp4', fourcc, 20, dims)
+dims = (256 << 3, 256 << 3)
+#fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+#vid = cv2.VideoWriter('output.mp4', fourcc, 20, dims)
 
 train = nnpy.TrainData(points = [
     nnpy.DataPoint(
@@ -29,9 +29,10 @@ net = nnpy.create_net([2, 5, 5, 1])
 loaded_train = train.load()
 
 
-def render(net, i):
+def render(net):
     out = np.ndarray((dims[0], dims[1], 3), dtype=np.uint8)
     for y in range(dims[1]):
+        print(y / dims[1])
         for x in range(dims[0]):
             u = x / dims[0]
             v = y / dims[1]
@@ -39,7 +40,7 @@ def render(net, i):
             p = int(nnpy.forward(net, [u, v])[0] * 255)
             out[x][y] = [p, p, p]
 
-    vid.write(out)
+    return out
 
 i = 0
 while True:
@@ -47,7 +48,8 @@ while True:
         loss = nnpy.train(net, loaded_train, rate)
         if i % 100 == 0:
             print(f"epoch: {i}, loss: {loss}")
-            render(net, i // 100)
+            #out = render(net, i // 100)
+            #vid.write(out)
         i += 1
     except KeyboardInterrupt:
         break
@@ -55,6 +57,7 @@ while True:
 cost = nnpy.loss(net, loaded_train)
 print(f"cost: {cost}")
 
-vid.release()
+#vid.release()
 
+imageio.imwrite('out.png', render(net))
 
